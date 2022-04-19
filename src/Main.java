3ee5f77458cd23dc5f333f9c3ec2c6a5ef832688
Tomek <Tomek@example.com>
2022-04-19 21:03:13 +0200

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,24 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        String month = "Kwiecień";
-        int year  = 2022;
-        long day = 19;
-        System.out.println("Miesiąc: " + month);
-        System.out.println("Rok: " + year);
-        System.out.println("Dzień: " + day);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Jak masz na imię? ");
+        String name = scanner.nextLine();
+        System.out.println("Cześć " + name);
+        System.out.println("A jak masz na nazwisko? ");
+        String surname = scanner.nextLine();
+        System.out.println("Dziękuję. A ile masz lat? ");
+        String age = scanner.nextLine();
+        System.out.println("Dziękuję. Miło się rozmawiało :)");
+
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+            int age = scanner.nextInt();
+
+            if (age > 18) {
+
+            }
+            }
     }
 }
