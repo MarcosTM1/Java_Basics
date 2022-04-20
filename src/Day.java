df380bfd7e1aa01adfd8fb0801d27958e3903eb0
Tomek <Tomek@example.com>
2022-04-20 18:51:27 +0200

    initial commit

---
+++ src/Day.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Day {
+    public static void main(String[] args) {
+//        /*1*/ int x = 3;
+//        /*2*/ int y;
+//
+//        /*3*/ System.out.println(x++); // = 3
+//        /*4*/ System.out.println(++x); // = 5
+//        /*5*/ System.out.println(x); // = 5
+//        /*6*/ y = x++; // y = 5 (6 w pamięci)
+//        /*7*/ System.out.println(y); // = 5
+//        /*8*/ y = ++x; // = 7
+//        /*9*/ System.out.println(y); // = 7
+//        /*10*/ System.out.println(++y); // = 8
+            int [] tablicaCyfr = new int[5];
+            tablicaCyfr = new int[]{1,2,3,4,5};
+            System.out.println(tablicaCyfr[2]);
+    }
+}
